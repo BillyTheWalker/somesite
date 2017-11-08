233682a7ccad9cbd6268618d0c198ce6c03cd555
prometej <nazar-danilyak@mail.ru>
2017-11-08 17:22:52 +0200

    add router controller

---
+++ web/src/main/java/com/developers/web/web/controller/Router.java
@@ -0,0 +1,15 @@
+package com.developers.web.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by danul on 08.11.2017.
+ */
+@Controller
+public class Router {
+    @GetMapping("/")
+    private String index() {
+        return "forward:/";
+    }
+}
